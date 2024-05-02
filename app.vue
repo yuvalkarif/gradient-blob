5ccc325ad2e615453afd4c240271da4d4fcdc564
Yuval Karif <yuval.k@monolith.co.il>
2024-05-02 21:25:21 +0300

    fix: eslint ready with daisy

--- app.vue
+++ app.vue
@@ -1,7 +1,24 @@
 <template>
-  <div>
-    <h1 class="text-2xl">
-      Welcome to Nuxt!
-    </h1>
+  <div class="  hero h-screen flex justify-center items-center">
+    <div class="hero-content flex-col prose">
+      <h1 class="font-black text-2xl">
+        Welcome to Nuxt!
+      </h1>
+      <div class="">
+        Preset made by @Yuval Karif
+      </div>
+      <h2>Powered By:</h2>
+      <article class="prose">
+        <ul>
+          asdf
+          <li>@eslint</li>
+          <li>@eslint</li>
+          <li>@eslint</li>
+        </ul>
+      </article>
+      <button class="btn btn-primary btn-sm">
+        Check it Out
+      </button>
+    </div>
   </div>
 </template>
